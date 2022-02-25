ae0fa0423d24c77b54005a486d6d2fe06d888a10
mino9421 <mino9421@gmail.com>
2022-02-24 19:04:33 -0500

    nav bar + profile + reservation
    reservation is inside of nav bar. I need to fix it by separating the files

--- BottomNavBar.js
+++ BottomNavBar.js
@@ -1,15 +1,84 @@
 import * as React from 'react'
-import { Text, View } from 'react-native'
+import axios from 'axios';
+import { Text, View, Button, TextInput, Alert  } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import CustomerProfile from './components/CustomerProfile';
 
 function HomeScreen() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
+
+  const [guests, onChangeGuests] = React.useState(null);
+  const [date, onChangeDate] = React.useState(null);
+  const [time, onChangeTime] = React.useState(null);
+  const [duration, onChangeDuration] = React.useState(null);
+
+  const submitData = () => {
+    console.log(JSON.stringify(guests))
+    console.log(JSON.stringify(date))
+    console.log(JSON.stringify(time))
+    console.log(JSON.stringify(duration))
+
+    var oldDate = new Date("Thu Jan 01 1970 21:30:00 GMT +0530(IST)");
+    var myData = {
+      reservation_maker: "61fed5dcd17185ff5269f018",
+      reservation_at: "62180a0de8b0487519699ceb",
+      numGuests: guests,
+      start: oldDate,
+      end: oldDate
+    }
+
+    axios.post("http://localhost:9090/api/v1/reservations", myData)
+      .then(function (response) {
+        console.log(response)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View>
+        <Text>Number of Guests</Text>
+        <TextInput
+          placeholder='Enter the number of guest'
+          // value={guests}
+          onChangeText={onChangeGuests}
+        />
+        <Text>Date</Text>
+        <TextInput
+          placeholder='dd/mm/yy'
+          value={date}
+          onChangeText={onChangeDate}
+        />
+        <Text>Time</Text>
+        <TextInput
+          placeholder='13:05'
+          value={time}
+          keyboardType='numeric'
+          onChangeText={onChangeTime}
+        />
+        <Text>Duration</Text>
+        <TextInput
+          placeholder='in minutes'
+          value={duration}
+          keyboardType='numeric'
+          onChangeText={onChangeDuration}
+        />
+        <Button
+          title="Reservation"
+          onPress={submitData}
+        />
       </View>
-    );
+      <View>
+        <Text>hi</Text>
+        <Text></Text>
+        <Text></Text>
+        <Text></Text>
+      </View>
+    </View>
+  );
 }
   
 function Login() {
@@ -31,7 +100,7 @@ export default function App() {
           inactiveColor="#3e2465"
           barStyle={{ backgroundColor: '#694fad' }}
         >
-          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Reservation" component={HomeScreen} />
           <Tab.Screen name="Login" component={Login} />
           <Tab.Screen name="Profile" component={CustomerProfile} />
         </Tab.Navigator>
